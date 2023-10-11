b73b2981d5ac6594bbcbe6bfddaa3ddd338384e4
Steffen Planthaber <steffen.planthaber@dfki.de>
2023-10-11 08:29:16 +0200

    add qt4/qt5 branch

--- viz/PluginLoader.cpp
+++ viz/PluginLoader.cpp
@@ -1,36 +1,30 @@
-#include "vizkit3d/Vizkit3DPlugin.hpp"
+#include "PluginLoader.hpp"
+
 #include "PCLPointCloudVisualization.hpp"
 #include "PolygonMeshVisualization.hpp"
 
 namespace vizkit3d {
-    class QtPluginVizkit : public vizkit3d::VizkitPluginFactory {
-    private:
-    public:
 
-        QtPluginVizkit() {
-        }
+    /**
+    * Returns a list of all available visualization plugins.
+    * @return list of plugin names
+    */
+    QStringList* QtPluginVizkitBase::getAvailablePlugins() const {
+        QStringList *pluginNames = new QStringList();
+        pluginNames->push_back("PCLPointCloudVisualization");
+        pluginNames->push_back("PolygonMeshVisualization");
+        return pluginNames;
+    }
 
-        /**
-        * Returns a list of all available visualization plugins.
-        * @return list of plugin names
-        */
-        virtual QStringList* getAvailablePlugins() const
-        {
-            QStringList *pluginNames = new QStringList();
-            pluginNames->push_back("PCLPointCloudVisualization");
-            pluginNames->push_back("PolygonMeshVisualization");
-            return pluginNames;
-        }
+    QObject* QtPluginVizkitBase::createPlugin(QString const& pluginName) {
+        if (pluginName == "PCLPointCloudVisualization")
+            return new vizkit3d::PCLPointCloudVisualization();
+        else if (pluginName == "PolygonMeshVisualization")
+            return new vizkit3d::PolygonMeshVisualization();
 
-        virtual QObject* createPlugin(QString const& pluginName)
-        {
-            vizkit3d::VizPluginBase* plugin = 0;
-            if (pluginName == "PCLPointCloudVisualization")
-                plugin = new vizkit3d::PCLPointCloudVisualization();
-            else if (pluginName == "PolygonMeshVisualization")
-                plugin = new vizkit3d::PolygonMeshVisualization();
-            return plugin;
-        };
-    };
-    Q_EXPORT_PLUGIN2(QtPluginVizkit, QtPluginVizkit)
+        return nullptr;
+    }
+    #if QT_VERSION < 0x050000
+        Q_EXPORT_PLUGIN2(QtPluginVizkitBase, QtPluginVizkitBase)
+    #endif
 }
